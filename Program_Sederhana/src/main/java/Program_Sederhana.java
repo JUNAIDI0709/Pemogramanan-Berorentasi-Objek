787264b2b1289abbad7e95dd22d7655bbf22ddc8
JUNAIDI0709 <161859492+JUNAIDI0709@users.noreply.github.com>
2024-03-02 14:44:22 +0700

    Add files via upload

---
+++ Program_Sederhana/src/main/java/Program_Sederhana.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Program_Sederhana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
